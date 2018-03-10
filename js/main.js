d7013c4938dcc933c79b7954bf76dbc32ec8579c
nidhal1993 <nidhal_mahdhi@yahoo.fr>
2018-03-10 15:02:06 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ function myFunction() {
 
 
 var imagenumber=1;
-
+setInterval (carousel,2000);
 
 function carousel(){
 	if (imagenumber>4){imagenumber=1;}
 	$('#test-image').attr('src',"resources/img"+imagenumber+".jpg");
 	imagenumber++;
-	setTimeout(carousel,2000);
+
 }
